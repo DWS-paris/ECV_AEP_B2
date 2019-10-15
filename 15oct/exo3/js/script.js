1489fb948f044b9a4430ae25c50ea84682c358fb
DWS-paris <j.noyer@digitalworkshop.fr>
2019-10-15 17:24:45 +0200

    Ajoouut de la requête media

--- 15oct/exo3/js/script.js
+++ 15oct/exo3/js/script.js
@@ -1,6 +1,7 @@
 /* 
 Délcaration d'un tableau d'objets
 */
+    let myList = document.querySelector('ul');
     let users = [
         {
             name: 'Chuck Norris',
@@ -19,4 +20,50 @@ Délcaration d'un tableau d'objets
             isHuman: true
         }
     ];
+//
+
+/* 
+Les conditions
+*/
+    if( 1 + 1 === 2 ){
+        console.log('Vrai');
+    }
+    else{
+        console.log('Faux');
+    };
+
+    1 + 1 === 4 ? console.log('Vrai') : console.log('Faux');
+//
+
+/* 
+Boucle sur une collection de données
+*/
+    for( let item of users ){
+        console.log( item.name );
+
+        if( item.isHuman && item.name !== 'Donald Trump' ){
+            console.log('Humain');
+            myList.innerHTML += `
+                <li>
+                    <i class="far fa-smile"></i> ${item.name}
+                </li>
+            `;
+        }
+        else if( item.name === 'Donald Trump' ){
+            console.log('Asshole');
+            myList.innerHTML += `
+                <li>
+                    <i class="fas fa-poop"></i> ${item.name}
+                </li>
+            `;
+        }
+        else{
+            console.log('Non humain');
+            myList.innerHTML += `
+                <li>
+                    <i class="fas fa-robot"></i> ${item.name}
+                </li>
+            `;
+        }
+    };
 //
